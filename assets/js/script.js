93f3137f86794995e534393381c9cb7285103259
fazle <ryan7998@gmail.com>
2021-01-09 16:41:06 -0500

    date feature added

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -107,13 +109,22 @@ $(".list-group").on("click", "span", function(){
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function(){
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element:
   dateInput.trigger("focus");
 
 })
 
 // Save after loosing focus from date:
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("change", "input[type='text']", function(){
   
   // get current text
   var date = $(this)
@@ -142,7 +153,28 @@ $(".list-group").on("blur", "input[type='text']", function(){
 
     // replace input with span element
     $(this).replaceWith(taskSpan);
+
+    
+    // Pass task's <li> element into auditTask() to check new due date
+    auditTask($(taskSpan).closest(".list-group-item"));
 })
+
+// Audit Date:
+var auditTask = function(taskEl) {
+  // get date from task element:
+  var date = $(taskEl).find("span").text().trim();
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date:
+  if(moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }else if(Math.abs(moment().diff(time, "days")) <= 2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // Make Sortable:
 $(".card .list-group").sortable({
@@ -209,6 +241,11 @@ $("#trash").droppable({
   out:function(event, ui){
     // console.log("out");
   }
+});
+
+// Date Picker:
+$("#modalDueDate").datepicker({
+  minDate:1
 });
 
 // modal was triggered
